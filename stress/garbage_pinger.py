692baab0e9294dc7b2d42c0688b0b0430d4f6316
memsdm05 <bnbrowner@gmail.org>
2020-06-16 17:44:32 -0400

    better gping

--- stress/garbage_pinger.py
+++ stress/garbage_pinger.py
@@ -5,18 +5,19 @@ import websockets
 import asyncio
 import random as r
 import string
-from datetime import datetime
 import sys
 import time
 import logging
 
-logging.basicConfig(format="")
+logging.basicConfig(format='[%(asctime)-15s][GPING] %(message)s', level=logging.INFO, stream=sys.stdout)
 
+average = 0
 count = 0
 chars = 0
 
-def timeprint(txt: str):
-    print(f'[{datetime.now().isoformat()}] {txt}')
+async def addToAverage(size, value):
+    global average
+    average = (size * average + value) / (size + 1)
 
 async def getGarbage(stringLength=8):
     lettersAndDigits = string.ascii_letters + string.digits
@@ -26,17 +27,25 @@ async def handler(uri):
     global count, chars
     async with websockets.connect(uri) as websocket:
         while True:
-            garbage = await getGarbage(r.randint(8, 30))
+            # Generate payload
+            garbage = await getGarbage(r.randint(10, 25))
 
+            # Start delta time measure. Send then receive payload. End delta time
             start = time.time()
             await websocket.send(garbage)
             back = await websocket.recv()
             end = time.time()
-            timeprint(f'SENT {garbage:>30}  RECEIVED {back:>30}  PING {round((end - start)*1000):>5}')
 
+            # Calculate ping. Increment count
+            ping = round((end - start)*1000)
             chars += len(garbage)
             count += 1
 
+            # Add to average and log
+            await addToAverage(count, ping)
+            logging.info(f'SENT {garbage:>25}  RECEIVED {back:>25}  PING {ping}')
+
+            # Sleepy boi
             await asyncio.sleep(r.random()*3)
 
 uri = sys.argv[1]
@@ -47,6 +56,7 @@ try:
         loop.create_task(handler(uri))
     loop.run_forever()
 except KeyboardInterrupt:
-    timeprint(f'SENT {chars} CHARACTERS {count} TIMES TO {uri}')
+    logging.info(f'SENT {chars} CHARACTERS {count} TIMES TO {uri}')
+    logging.info(f'AVERAGE PING: {round(average)}')
 except websockets.exceptions.ConnectionClosedError:
-    timeprint('CONNECTION CLOSED ABNORMALLY')
+    logging.info('CONNECTION CLOSED ABNORMALLY')
