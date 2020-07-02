7696bb70de11b294f47419492f29677f889f8e0b
memsdm05 <22browner@da.org>
2020-07-01 21:55:33 -0400

    GPING added message length argparse

--- stress/gping.py
+++ stress/gping.py
@@ -25,12 +25,10 @@ class GarbagePingerProtocol(WebSocketClientProtocol):
     def onConnect(self, response):
         watch = PingStopwatch()
 
-
     def onOpen(self):
 
         def ping():
             self.sendMessage("Hello, world!".encode('utf8'))
-            self.sendMessage(b"\x00\x01\x03\x04", isBinary=True)
             self.factory.loop.call_later(r.random()*3, ping)
 
         # start sending messages every second ..
@@ -49,7 +47,7 @@ class GarbagePingerProtocol(WebSocketClientProtocol):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='A websocket mass pinger for testing purposes')
 
-    parser.add_argument('--port', '-p', dest='port', type=int, default=4000,
+    parser.add_argument('--port', '-P', dest='port', type=int, default=4000,
                         help='The port to connect to (default: 4000)')
     parser.add_argument('--ip', '-I', dest='ip', type=str, default='localhost',
                         help='The uri to connect to (default: localhost)')
@@ -60,32 +58,40 @@ if __name__ == '__main__':
     parser.add_argument('--threads', '-t', dest='threads', type=int, default=0,
                         help='How many threads to run the connections in, 0 for no threads (default: 0')
 
-    parser.add_argument('--highest-delay', '-H', dest='high', type=int, default=3)
-    parser.add_argument('--lowest-delay', '-L', dest='low', type=int, default=0)
-    parser.add_argument('--integer-random', 'r', action='store_true', default=False)
-    parser.add_argument('--no-random-delay', '-n', action='store_true', default=False,
+
+    parser.add_argument('--highest-delay', '-D', dest='high', type=int, default=3)
+    parser.add_argument('--lowest-delay', '-d', dest='low', type=int, default=0)
+    parser.add_argument('--integer-random', '-r', dest='int_rand', action='store_true', default=False,
+                        help='will use random.randint instead of uniform to set delay times')
+    parser.add_argument('--no-random-delay', '-N', dest='no_random_delay', action='store_true', default=False,
                         help='Sets the delay to only delay at lowest delay')
+
+    parser.add_argument('--highest-length', '-L', dest='high', type=int, default=10)
+    parser.add_argument('--lowest-length', '-l', dest='low', type=int, default=30)
+    parser.add_argument('--no-random-length', '-n', dest='no_random_length', action='store_true', default=False,
+                        help='Sets the message length to lowest length set')
     parser.add_argument('--binary', '-b', action='store_true', default=False,
                         help='Sends random bytes instead of random strings')
-    # TODO Add message length arguments (random, no length)
-    parser.add_argument('--do-not-measure-ping', '-p', action='store_true', default=False,
+
+    parser.add_argument('--do-not-measure-ping', '-p', dest='no_ping', action='store_true', default=False,
                         help='Do not measure ping')
-    parser.add_argument('--run_once', '-o', dest='once', action='store_true', default=False,
+    parser.add_argument('--run-once', '-o', dest='once', action='store_true', default=False,
                         help='Ping once then die like the crap that you are')
     parser.add_argument('--message', '-m', dest='message', type=str, default='',
                         help='Send this message instead of random garbage')
-    parser.add_argument('--input', 'i', dest='input', type=str, default='',
+    parser.add_argument('--input', '-i', dest='input', type=str, default='',
                         help='Read a control file and create subprocess (IO is disabled)')
-    parser.add_argument('--output' 'o', dest='output', type=str, default='',
+    parser.add_argument('--output' '-o', dest='output', type=str, default='',
                         help='Outputs data to a specified text files')
 
     args = parser.parse_args()
+
 
     factory = WebSocketClientFactory()
     factory.protocol = GarbagePingerProtocol
 
     loop = asyncio.get_event_loop()
-    coro = loop.create_connection(factory, '127.0.0.1', 4000)
-    loop.run_until_complete(coro)
+    for _ in range():
+        loop.run_until_complete(loop.create_connection(factory, '127.0.0.1', 4000))
     loop.run_forever()
     loop.close()
